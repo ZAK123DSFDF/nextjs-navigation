c60a801da5607531770d6dc6110ea695cb97b04b
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-03-21 18:19:23 +0300

    second commit

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
 export default function ProfileId() {
   const params = useParams();
+  const router = useRouter();
   useEffect(() => console.log(params), [params]);
   return (
     <>
       <div>this is profileId:{params.id}</div>
-      <Link href={"/"}>go to home</Link>
+      <button onClick={() => router.push("/")}>go to home </button>
     </>
   );
 }
